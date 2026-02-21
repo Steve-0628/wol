d0eb42f1814363a5e0d31eb6714e59f1eb49fae3
Steve-0628 <49326405+Steve-0628@users.noreply.github.com>
2026-02-22 06:36:42 +0900

    byebye nextjs

--- src/app/api/route.tsx
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(req: Request): Promise<NextResponse> {
-  return NextResponse.json({ message: 'Hello World!' });
-}
--- src/app/api/wol/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import wol from 'wake_on_lan';
-
-export async function POST(req: Request): Promise<NextResponse> {
-  const { mac } = await req.json();
-  wol.wake(mac);
-  return NextResponse.json({ message: 'Hello WoL!' });
-}
-
